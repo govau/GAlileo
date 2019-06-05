4b859bcd409b782d563ffbddf38d9a6a2060a837
Alexander Sadleir <alex.sadleir@digital.gov.au>
2019-06-06 09:08:41 +1000

    split shiny code into seperate files

---
+++ shiny/observatory/userjourneys.R
@@ -0,0 +1,16 @@
+userJourneys <- tabPanel(
+  "User Journeys", # Sidebar with a slider input for number of bins
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+    )
+    ,
+    # Main panel for displaying outputs ----
+    main = mainPanel(
+    )
+  )
+)
+
+userjourneys_server <- function (input, output) {
+}
