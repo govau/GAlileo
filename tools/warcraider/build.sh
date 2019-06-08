d5f490d5f612ae373fb06cbf690937e66b1b2302
Alex Sadleir <maxious@lambdacomplex.org>
2019-06-08 10:33:12 +1000

    center text labels

--- tools/warcraider/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# need to run "gcloud auth configure-docker" once
-docker build -t warcraider . && docker tag warcraider gcr.io/dta-ga-bigquery/warcraider && docker push gcr.io/dta-ga-bigquery/warcraider
--- tools/warcraider/run.bat
+++
@@ -1,7 +0,0 @@
-SET RUSTFLAGS=-C target-cpu=native -C link-args=/STACK:4194304
-SET RUST_BACKTRACE=1
-SET RUST_LOG=warcraider=info
-SET REPLICAS=8
-SET OFFSET=1
-cargo run --release %*
-pause
