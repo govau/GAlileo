1a82647a35f741976379f662b1e5b6dc414a2b1f
Alex Sadleir <maxious@lambdacomplex.org>
2019-06-08 10:33:12 +1000

    center text labels

--- shiny/observatory/siteuserjourneys.js
+++ shiny/observatory/siteuserjourneys.js
@@ -1,82 +1,78 @@
-// !preview r2d3 data=list(data.frame(title = c("DTA","About Us","Join Our Team","Recruiterbox"),                      href=c("https://google.com","https://google.com","https://google.com","")),           data.frame(title = c("DTA","Blogs", "Help and Advice"),                      href=c("https://google.com","https://google.com","https://google.com")),           data.frame(title = c("Domain Names","Guidelines", "Name Server Change"),                      href=c("https://google.com","https://google.com","https://google.com")),           data.frame(title = c("Design System","Components", "Templates"),                      href=c("https://google.com","https://google.com","https://google.com")),           data.frame(title = c("Design System","Get Started", "Download", "Community"),                      href=c("https://google.com","https://google.com","https://google.com", ""))      ),height=800,width="100%"
-//
-// r2d3: https://rstudio.github.io/r2d3
+// !preview r2d3 data=list(data.frame(title = c("DTA","About Us","Join Our Team","Recruiterbox"), href=c("https://google.com","https://google.com","https://google.com","")),           data.frame(title = c("DTA","Blogs", "Help and Advice"),                      href=c("https://google.com","https://google.com","https://google.com")),           data.frame(title = c("Domain Names","Guidelines", "Name Server Change"),                      href=c("https://google.com","https://google.com","https://google.com")),           data.frame(title = c("Design System","Components", "Templates"),                      href=c("https://google.com","https://google.com","https://google.com")),           data.frame(title = c("Design System","Get Started", "Download", "Community"),                      href=c("https://google.com","https://google.com","https://google.com", ""))      ),height=800,width="100%"
 //
 
 r2d3.svg.selectAll("svg > *").remove();
-var g = r2d3.svg.selectAll()
-    .data(data)
+var g = r2d3.svg
+  .selectAll()
+  .data(data)
   .enter()
   .append("g")
-    .attr("transform", function(d, i) {return "translate(" + 15 + "," + (i*140) + ")"});
+  .attr("transform", function(d, i) {
+    return "translate(" + 15 + "," + i * 140 + ")";
+  });
 
-function dx(d,i) {
-   return (i * 145) + 50;
+function dx(d, i) {
+  return i * 145 + 50;
 }
-function dy(d,i) {
-   return 90;
+function dy(d, i) {
+  return 90;
 }
-var lineFunction = d3.line()
-                         .x(function(d,i) {return dx(d,i)})
-                         .y(function(d,i) {return dy(d,i)})
-                         .curve(d3.curveLinear);
-var line = g.append("path")
-                            .attr("d", function(d) {return lineFunction(d.title)})
-                            .attr("stroke", "grey")
-                            .attr("stroke-width", 20)
-                            .attr("fill", "white");
+var lineFunction = d3
+  .line()
+  .x(function(d, i) {
+    return dx(d, i);
+  })
+  .y(function(d, i) {
+    return dy(d, i);
+  })
+  .curve(d3.curveLinear);
+var line = g
+  .append("path")
+  .attr("d", function(d) {
+    return lineFunction(d.title);
+  })
+  .attr("stroke", "grey")
+  .attr("stroke-width", 20)
+  .attr("fill", "white");
 
-var circles = g.selectAll().data(function (d) {
+var circles = g.selectAll().data(function(d) {
   return HTMLWidgets.dataframeToD3(d);
 });
-circles.enter()
-    .append('circle')
-    .attr("cy", function(d,i) {return dy(d,i)})
-    .attr("cx", function(d,i) {return dx(d,i)})
-      .attr('r',30)
-      .attr('fill', 'white')
-      .attr('stroke', 'grey')
-      .attr('stroke-width','20px')
-      ;
+circles
+  .enter()
+  .append("circle")
+  .attr("cy", function(d, i) {
+    return dy(d, i);
+  })
+  .attr("cx", function(d, i) {
+    return dx(d, i);
+  })
+  .attr("r", 30)
+  .attr("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", "20px");
 
-function wrap(text, width) {
-  // https://stackoverflow.com/questions/24784302/wrapping-text-in-d3
-    text.each(function () {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            x = text.attr("x"),
-            y = text.attr("y"),
-            dy = 0, //parseFloat(text.attr("dy")),
-            tspan = text.text(null)
-                        .append("tspan")
-                        .attr("x", x)
-                        .attr("y", y)
-                        .attr("dy", dy + "em");
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan")
-                            .attr("x", x)
-                            .attr("y", y)
-                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                            .text(word);
-            }
-        }
-    });
-}
-var texts = g.selectAll().data(function(d) { return HTMLWidgets.dataframeToD3(d)}).enter()
-      .append("a").attr("href",function(d) { return d.href })
-      .append("text")
-      .text(function(d) { return d.title })
-      .style("font-size", function(d,i) { return Math.min(20, dy(d,i)/ this.getComputedTextLength() * 20) + "px"; })
-      .attr("dx", function(d,i) {return dx(d,i)-this.getComputedTextLength()/2})
-      .attr("dy", "160px");
-
+var texts = g
+  .selectAll()
+  .data(function(d) {
+    return HTMLWidgets.dataframeToD3(d);
+  })
+  .enter()
+  .append("foreignObject")
+  .attr("x", function(d, i) {
+    return dx(d, i)-dy(d,i)/2;
+  })
+  .attr("y", function(d, i) {
+    return dy(d, i)+ (dy(d, i)/4)+5;
+  })
+  .attr("width", function(d, i) {
+    return dy(d, i);
+    })
+  .attr("height", function(d, i) {
+    return dy(d, i)/2;
+    })
+  .append("xhtml:p")
+  .style("text-align","center")
+  .html(function(d) {
+    return d.title;
+  });
