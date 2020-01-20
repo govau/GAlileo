ee75306e0eb420e8b65250ec625e63a77cead15b
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-01-20 15:30:38 +1100

    Automate BigQuery Script
    1- Updated schedule with Australia/Sydney timezome
    2- Added BigQuery SQL for execution in data pipeline

--- dags/ga_pgvw_daily_snapshot_emp.py
+++ dags/ga_pgvw_daily_snapshot_emp.py
@@ -1,12 +1,17 @@
 from __future__ import print_function
 import datetime
+import pendulum
 import os
 import tablib
+import pathlib
 
 from airflow import models
 from airflow.operators import python_operator
 from airflow.contrib.operators import bigquery_to_gcs
 from airflow.contrib.operators import bigquery_operator
+
+from google.cloud import bigquery
+
 
 from galileo import galileo, searchconsole, ga
 
@@ -15,32 +20,20 @@ default_dag_args = {
     # fixed point in time rather than dynamically, since it is evaluated every
     # time a DAG is parsed. See:
     # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    'start_date': datetime.datetime(2020, 1, 15),
+    # 'start_date': datetime.datetime(2020, 1, 15),
+    'start_date': pendulum.create(2020, 1, 15, tz="Australia/Sydney")
 }
 
 with models.DAG(
         'pageviews_snapshot',
         schedule_interval=datetime.timedelta(days=1),
+        # schedule_interval = '0 0 * * *',
         default_args=default_dag_args) as dag:
     project_id = models.Variable.get('GCP_PROJECT', 'dta-ga-bigquery')
-    # day = (datetime.date.today() ).strftime("%Y%m%d")
-
-    # query = """
-    #     CREATE TABLE `{{params.project_id}}.tmp.{{params.temp_table}}_{{ ts_nodash }}`
-    #     OPTIONS(
-    #       expiration_timestamp=TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 1 HOUR)
-    #     ) AS
-
-    #                """
-
-    # query_pageviews_snapshot = bigquery_operator.BigQueryOperator(
-    #     task_id='query_pageviews_snapshot',
-    #     bql=query, use_legacy_sql=False, params={
-    #         'project_id': project_id,
-    #         'start': start,
-    #         'end': end,
-    #         'search_param': d['search_param']
-    #     })
+  
+    query_pageviews_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_pageviews_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_pgvw_daily_snapshot_full").read_text(), use_legacy_sql=False)
     export_pageviews_snapshot_to_gcs = bigquery_to_gcs.BigQueryToCloudStorageOperator(
         task_id='export_internalsearch_to_gcs',
         source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_emp",
@@ -53,5 +46,4 @@ with models.DAG(
                                     'us-east1-dta-airflow-b3415db4-bucket'),
                 'pgviews_daily_snapshot_emp')],
         export_format='NEWLINE_DELIMITED_JSON')
-    # query_pageviews_snapshot >>
-    export_pageviews_snapshot_to_gcs
+    query_pageviews_snapshot >> export_pageviews_snapshot_to_gcs
