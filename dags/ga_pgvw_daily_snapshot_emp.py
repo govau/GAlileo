0416be65cc348fefe0567d083be1df837443b33a
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-02-11 15:14:21 +1100

    Update employment DAG for device browser script and data delivery

--- dags/ga_pgvw_daily_snapshot_emp.py
+++ dags/ga_pgvw_daily_snapshot_emp.py
@@ -59,7 +59,15 @@ with models.DAG(
         task_id='query_device_category_delta_snapshot',
         bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicecategory_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
 
+    # device browser snapshot
+    query_device_browser_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_device_browser_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicebrowser_daily_full_snapshot_emp").read_text(), use_legacy_sql=False)
 
+    query_device_browser_delta_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_device_browser_delta_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicebrowser_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
+    # ============================================================================================================
     # Export datasets
     # pageviews snapshot
     export_bq_to_gcs_json = bigquery_to_gcs.BigQueryToCloudStorageOperator(
@@ -114,7 +122,7 @@ with models.DAG(
                                 'us-east1-dta-airflow-b3415db4-bucket'),
             'totalvisitors_daily_snapshot_emp')],
     export_format='CSV')
-
+    
     # device category snapshot
     export_bq_to_gcs_json_device_category = bigquery_to_gcs.BigQueryToCloudStorageOperator(
         task_id='export_bq_to_gcs_json_device_category',
@@ -141,10 +149,39 @@ with models.DAG(
                                 'us-east1-dta-airflow-b3415db4-bucket'),
             'device_category_daily_snapshot_emp')],
     export_format='CSV')
-    
+
+    # device browser snapshot
+    export_bq_to_gcs_json_device_browser = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+        task_id='export_bq_to_gcs_json_device_browser',
+        source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_device_browser_delta_emp",
+        params={
+            'project_id': project_id
+        },
+        destination_cloud_storage_uris=[
+            "gs://%s/data/analytics/json/%s.json" % (
+                models.Variable.get('AIRFLOW_BUCKET',
+                                    'us-east1-dta-airflow-b3415db4-bucket'),
+                'device_browser_daily_snapshot_emp')],
+        export_format='NEWLINE_DELIMITED_JSON')
+
+    export_bq_to_gcs_csv_device_browser = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+    task_id='export_bq_to_gcs_csv_device_browser',
+    source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_device_browser_delta_emp",
+    params={
+        'project_id': project_id
+    },
+    destination_cloud_storage_uris=[
+        "gs://%s/data/analytics/csv/%s.csv" % (
+            models.Variable.get('AIRFLOW_BUCKET',
+                                'us-east1-dta-airflow-b3415db4-bucket'),
+            'device_browser_daily_snapshot_emp')],
+    export_format='CSV')
+    # ============================================================================================================
     query_pageviews_snapshot >> query_pageviews_snapshot_delta >> export_bq_to_gcs_json
     query_pageviews_snapshot_delta >> export_bq_to_gcs_csv
     query_total_visitors_snapshot >> query_total_visitors_delta_snapshot >> export_bq_to_gcs_json_totalvisitors
     query_total_visitors_delta_snapshot >> export_bq_to_gcs_csv_totalvisitors
     query_device_category_snapshot >> query_device_category_delta_snapshot >> export_bq_to_gcs_json_device_category
     query_device_category_delta_snapshot >> export_bq_to_gcs_csv_device_category
+    query_device_browser_snapshot >> query_device_browser_delta_snapshot >> export_bq_to_gcs_json_device_browser
+    query_device_browser_delta_snapshot >> export_bq_to_gcs_csv_device_browser
