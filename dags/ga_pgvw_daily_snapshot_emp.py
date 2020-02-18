95d1c787d4492461a67a44f4171aa26b9960641d
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2020-02-18 12:51:10 +1100

    Added Datapipeline in workflow
    Device browser
    Device operating system
    Device category
    Traffic source and medium
    Traffic source geolocation - local cities and countries
    User engagement measure

--- dags/ga_pgvw_daily_snapshot_emp.py
+++ dags/ga_pgvw_daily_snapshot_emp.py
@@ -53,7 +53,7 @@ with models.DAG(
     # device category snapshot
     query_device_category_snapshot = bigquery_operator.BigQueryOperator(
         task_id='query_device_category_snapshot',
-        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicecategory_daily_full_snapshot_emp").read_text(), use_legacy_sql=False)
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicecategory_daily_snapshot_full_emp").read_text(), use_legacy_sql=False)
 
     query_device_category_delta_snapshot = bigquery_operator.BigQueryOperator(
         task_id='query_device_category_delta_snapshot',
@@ -62,7 +62,7 @@ with models.DAG(
     # device browser snapshot
     query_device_browser_snapshot = bigquery_operator.BigQueryOperator(
         task_id='query_device_browser_snapshot',
-        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicebrowser_daily_full_snapshot_emp").read_text(), use_legacy_sql=False)
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_devicebrowser_daily_snapshot_full_emp").read_text(), use_legacy_sql=False)
 
     query_device_browser_delta_snapshot = bigquery_operator.BigQueryOperator(
         task_id='query_device_browser_delta_snapshot',
@@ -71,11 +71,42 @@ with models.DAG(
     # device operating system snapshot
     query_device_ops_snapshot = bigquery_operator.BigQueryOperator(
         task_id='query_device_ops_snapshot',
-        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_deviceops_daily_full_snapshot_emp").read_text(), use_legacy_sql=False)
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_deviceops_daily_snapshot_full_emp").read_text(), use_legacy_sql=False)
 
     query_device_ops_delta_snapshot = bigquery_operator.BigQueryOperator(
         task_id='query_device_ops_delta_snapshot',
-        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_deviceops_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
+        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts/dta_sql_deviceops_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
+        
+    # traffic source and medium snapshot
+    query_traffic_src_medium_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_traffic_src_medium_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_traffic_sourcemedium_daily_snapshot_full_emp").read_text(), use_legacy_sql=False)
+
+    query_traffic_src_medium_delta_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_traffic_src_medium_delta_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts/dta_sql_traffic_sourcemedium_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
+
+    # local city and country snapshot
+    query_geolocation_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_geolocation_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR+"/bq_scripts/dta_sql_geolocation_daily_snapshot_full_emp").read_text(), use_legacy_sql=False)
+
+    query_local_city_delta_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_local_city_delta_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts/dta_sql_geolocation_localcity_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
+
+    query_country_delta_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_country_delta_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts/dta_sql_geolocation_country_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
+
+    # user session level engagement snapshot
+    query_user_session_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_user_session_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts/dta_sql_sessions_user_daily_snapshot_full_emp").read_text(), use_legacy_sql=False)
+        
+    query_user_session_delta_snapshot = bigquery_operator.BigQueryOperator(
+        task_id='query_user_session_delta_snapshot',
+        bql=pathlib.Path(galileo.DAGS_DIR + "/bq_scripts/dta_sql_sessions_user_daily_snapshot_delta_emp").read_text(), use_legacy_sql=False)
     # ============================================================================================================
     # Export datasets
     # pageviews snapshot
@@ -212,6 +243,114 @@ with models.DAG(
                                 'us-east1-dta-airflow-b3415db4-bucket'),
             'device_ops_daily_snapshot_emp')],
     export_format='CSV')
+
+     # traffic source and medium snapshot
+    export_bq_to_gcs_json_traffic_src_medium = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+        task_id='export_bq_to_gcs_json_traffic_src_medium',
+        source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_traffic_src_medium_delta_emp",
+        params={
+            'project_id': project_id
+        },
+        destination_cloud_storage_uris=[
+            "gs://%s/data/analytics/json/%s.json" % (
+                models.Variable.get('AIRFLOW_BUCKET',
+                                    'us-east1-dta-airflow-b3415db4-bucket'),
+                'traffic_src_medium_daily_snapshot_emp')],
+        export_format='NEWLINE_DELIMITED_JSON')
+
+    export_bq_to_gcs_csv_traffic_src_medium = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+    task_id='export_bq_to_gcs_csv_traffic_src_medium',
+    source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_traffic_src_medium_delta_emp",
+    params={
+        'project_id': project_id
+    },
+    destination_cloud_storage_uris=[
+        "gs://%s/data/analytics/csv/%s.csv" % (
+            models.Variable.get('AIRFLOW_BUCKET',
+                                'us-east1-dta-airflow-b3415db4-bucket'),
+            'traffic_src_medium_daily_snapshot_emp')],
+    export_format='CSV')
+
+    # local city snapshot
+    export_bq_to_gcs_json_local_city = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+        task_id='export_bq_to_gcs_json_local_city',
+        source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_local_city_delta_emp",
+        params={
+            'project_id': project_id
+        },
+        destination_cloud_storage_uris=[
+            "gs://%s/data/analytics/json/%s.json" % (
+                models.Variable.get('AIRFLOW_BUCKET',
+                                    'us-east1-dta-airflow-b3415db4-bucket'),
+                'local_city_daily_snapshot_emp')],
+        export_format='NEWLINE_DELIMITED_JSON')
+
+    export_bq_to_gcs_csv_local_city = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+    task_id='export_bq_to_gcs_csv_local_city',
+    source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_local_city_delta_emp",
+    params={
+        'project_id': project_id
+    },
+    destination_cloud_storage_uris=[
+        "gs://%s/data/analytics/csv/%s.csv" % (
+            models.Variable.get('AIRFLOW_BUCKET',
+                                'us-east1-dta-airflow-b3415db4-bucket'),
+            'local_city_daily_snapshot_emp')],
+    export_format='CSV')
+
+    # Country snapshot
+    export_bq_to_gcs_json_country = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+        task_id='export_bq_to_gcs_json_country',
+        source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_country_delta_emp",
+        params={
+            'project_id': project_id
+        },
+        destination_cloud_storage_uris=[
+            "gs://%s/data/analytics/json/%s.json" % (
+                models.Variable.get('AIRFLOW_BUCKET',
+                                    'us-east1-dta-airflow-b3415db4-bucket'),
+                'country_daily_snapshot_emp')],
+        export_format='NEWLINE_DELIMITED_JSON')
+
+    export_bq_to_gcs_csv_country = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+        task_id='export_bq_to_gcs_csv_country',
+        source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_country_delta_emp",
+        params={
+        'project_id': project_id
+        },
+        destination_cloud_storage_uris=[
+            "gs://%s/data/analytics/csv/%s.csv" % (
+                models.Variable.get('AIRFLOW_BUCKET',
+                                'us-east1-dta-airflow-b3415db4-bucket'),
+            'country_daily_snapshot_emp')],
+    export_format='CSV')
+
+    # user session level engagement snapshot
+    export_bq_to_gcs_json_session_users = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+        task_id='export_bq_to_gcs_json_session_users',
+        source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_session_user_delta_emp",
+        params={
+            'project_id': project_id
+        },
+        destination_cloud_storage_uris=[
+            "gs://%s/data/analytics/json/%s.json" % (
+                models.Variable.get('AIRFLOW_BUCKET',
+                                    'us-east1-dta-airflow-b3415db4-bucket'),
+                'session_users_daily_snapshot_emp')],
+        export_format='NEWLINE_DELIMITED_JSON')
+
+    export_bq_to_gcs_csv_session_users = bigquery_to_gcs.BigQueryToCloudStorageOperator(
+    task_id='export_bq_to_gcs_csv_session_users',
+    source_project_dataset_table="{{params.project_id}}.dta_customers.pageviews_daily_snapshot_session_user_delta_emp",
+    params={
+        'project_id': project_id
+    },
+    destination_cloud_storage_uris=[
+        "gs://%s/data/analytics/csv/%s.csv" % (
+            models.Variable.get('AIRFLOW_BUCKET',
+                                'us-east1-dta-airflow-b3415db4-bucket'),
+            'session_users_daily_snapshot_emp')],
+    export_format='CSV')
     # ============================================================================================================
     query_pageviews_snapshot >> query_pageviews_snapshot_delta >> export_bq_to_gcs_json
     query_pageviews_snapshot_delta >> export_bq_to_gcs_csv
@@ -223,3 +362,11 @@ with models.DAG(
     query_device_browser_delta_snapshot >> export_bq_to_gcs_csv_device_browser
     query_device_ops_snapshot >> query_device_ops_delta_snapshot >> export_bq_to_gcs_json_device_ops
     query_device_ops_delta_snapshot >> export_bq_to_gcs_csv_device_ops
+    query_traffic_src_medium_snapshot >> query_traffic_src_medium_delta_snapshot >> export_bq_to_gcs_json_traffic_src_medium
+    query_traffic_src_medium_delta_snapshot >> export_bq_to_gcs_csv_traffic_src_medium
+    query_geolocation_snapshot >> query_local_city_delta_snapshot >> export_bq_to_gcs_json_local_city
+    query_local_city_delta_snapshot >> export_bq_to_gcs_csv_local_city
+    query_geolocation_snapshot >> query_country_delta_snapshot >> export_bq_to_gcs_json_country
+    query_country_delta_snapshot >> export_bq_to_gcs_csv_country
+    query_user_session_snapshot >> query_user_session_delta_snapshot >> export_bq_to_gcs_json_session_users
+    query_user_session_delta_snapshot >> export_bq_to_gcs_csv_session_users
