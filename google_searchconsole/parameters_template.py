06734217b81b834d204a0d4bdfee33463b86602f
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2021-02-26 12:20:30 +1100

    template parameters for Google search console and BigQuery

---
+++ google_searchconsole/parameters_template.py
@@ -0,0 +1,9 @@
+
+PROPERTIES = ["https://www.<hostname1>.gov.au",
+              "https://<hostname2>.gov.au", "https://<hostname3>.gov.au/"]
+BQ_DATASET_NAME = '<dataset_name>'
+BQ_TABLE_NAME = '<table_name>'
+BQ_TABLE_NAME_TEST = '<testing table name>'
+SERVICE_ACCOUNT_FILE = '<credentials filename>.json'
+
+SCOPES = ['https://www.googleapis.com/auth/webmasters']
