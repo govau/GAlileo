ffadeda9234fad7f981f545f9dba6e680ff98606
nikicrow <72773639+nikicrow@users.noreply.github.com>
2021-06-01 21:06:05 -0700

    added comments to the hotdog extract sql file

--- ga4_analysis/hotdog_extract.sql
+++ ga4_analysis/hotdog_extract.sql
@@ -1,24 +1,42 @@
 -- Let's create the matrix that we need for a logistic regression
+-- to have aggregated event data and also to have the demographic user data is apparently really tricky, 
+-- so i have dont the aggregation on the user data in python to save time and stress
 
 SELECT
+  -- the data will be aggregated by user_pseudo_id so we will look at users and what made them engaged
   user_pseudo_id,
+  -- We will aggregate the event data for each user. The events we will be focusing on will be:
+  -- link clicks
   sum(if(event_name = 'link_click',1,0)) as count_link_click,
+  -- scroll events
   sum(if(event_name = 'scroll',1,0)) as count_scroll,
+  -- first visit events
   sum(if(event_name = 'first_visit',1,0)) as count_first_visit,
+  -- clicks
   sum(if(event_name = 'click',1,0)) as count_click,
+  -- pageviews
   sum(if(event_name = 'pageview',1,0)) as count_pageview,
+  -- external clicks
   sum(if(event_name = 'external_click',1,0)) as count_external_click,
+  -- file downloads
   sum(if(event_name = 'file_download',1,0)) as count_file_download,
+  -- user engagement events - i still am not entirely sure what these are and if they will mess with the analysis
   sum(if(event_name = 'user_engagement',1,0)) as count_user_engagement,
-    -- event names link_click, scroll, first_visit, click, pageview, external_click, file_download, user_engagement
   --case when event_param_key= 'page_title' then event_param_value else null end as page_title,
+  -- each user may or may not have a session_engaged parameter within an event - yes this is not actually session specific apparently
   sum(session_engaged) as sum_engaged_count,
   count(session_engaged) as session_engaged_count
+  -- you can comment the above or below to see the engaged session counts and sums to demonstrate the point i make
+  -- we will use engagement rate as the thing we are trying to predict
+  -- sum(session_engaged)/count(session_engaged) as engagement_rate
 FROM(
   SELECT
+    -- we basically need this subquery to define the session_engaged metric, if you need to do anything to the key value params, do it here
     event_date, 
     user_pseudo_id,
     event_name,
+    -- apparently when session_engaged triggers, sometimes it is in the int_value column, and sometimes it is in string_value?
+    -- I got around this by adding them and casting the string to an integer
     case when params.key= 'session_engaged' 
          then ifnull(params.value.int_value,0) + ifnull(cast(params.value.string_value as int64),0) 
          else null end as session_engaged
@@ -35,7 +53,8 @@ GROUP BY
 user_pseudo_id
 ;
 
--- make a seperate table for the device category, browser and country so that we can join them on in python after finding the max
+-- I tried really hard to do this in bigquery but a few of the functions that could help do not work in bigquery :(
+-- This is a seperate table for the device category, browser and country so that we can join them on in python after finding the mode
 select
   user_pseudo_id,
   event_name,
