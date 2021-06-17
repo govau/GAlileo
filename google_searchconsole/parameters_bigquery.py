0c18ecb2b332d887286462b2f316e46313e93182
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2021-06-17 16:52:10 +1000

    Google Search Console data ingest into BigQuery
    Python 3 script to extract transform load Google Search Console datasets of verified sites into BigQuery for analytics

---
+++ google_searchconsole/parameters_bigquery.py
@@ -0,0 +1,8 @@
+
+PROPERTIES = ["<url 1>", "<url 2>"]
+BQ_DATASET_NAME = '<dataset name>'
+BQ_TABLE_NAME = '<table name>'
+CLIENT_SECRET_FILE = '<client_secrets>.json'
+SERVICE_ACCOUNT_FILE_BQ = '<name>.json'
+
+SCOPES = ['https://www.googleapis.com/auth/webmasters']
