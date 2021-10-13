cfd9a5a2918fec38f0cc6dbfce15435e004706b0
mufaddal <mufaddal.taiyab-ali@dta.gov.au>
2021-10-13 16:11:07 +1100

    Customise Google Analytics billable report script

--- dags/bq_scripts_ga_usage_billable/ga_billable_hits_agency_report.sql
+++ dags/bq_scripts_ga_usage_billable/ga_billable_hits_agency_report.sql
@@ -1,7 +1,56 @@
 -- BigQuery script for producing each agency billable hits report based on month aggregation
 -- Data extract from GA360 account in csv format and loaded into BigQuery tables
 
-
+SELECT 
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'JULY 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202007_report_detail` 
+WHERE agency_name = 'HA'
+union all
+SELECT 
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'AUGUST 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202008_report_detail` 
+WHERE agency_name = 'HA'
+union all
+SELECT 
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'SEPTEMBER 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202009_report_detail` 
+WHERE agency_name = 'HA'
+union all
+SELECT 
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'OCTOBER 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202010_report_detail` 
+WHERE agency_name = 'HA'
+union all
+SELECT 
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'NOVEMBER 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202011_report_detail` 
+WHERE agency_name = 'HA'
+union all
 SELECT 
 agency_name,
 ga_id,
@@ -10,7 +59,7 @@ total_service_hits,
 total_agency_hits,
 'DECEMBER 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202012_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -20,7 +69,7 @@ total_service_hits,
 total_agency_hits,
 'JANUARY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202101_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -30,7 +79,7 @@ total_service_hits,
 total_agency_hits,
 'FEBRUARY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202102_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -40,7 +89,7 @@ total_service_hits,
 total_agency_hits,
 'MARCH 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202103_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -50,7 +99,7 @@ total_service_hits,
 total_agency_hits,
 'APRIL 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202104_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -60,7 +109,7 @@ total_service_hits,
 total_agency_hits,
 'MAY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202105_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -70,7 +119,7 @@ total_service_hits,
 total_agency_hits,
 'JUNE 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202106_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -80,7 +129,7 @@ total_service_hits,
 total_agency_hits,
 'JULY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202107_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -90,7 +139,7 @@ total_service_hits,
 total_agency_hits,
 'AUGUST 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202108_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA'
 union all
 SELECT 
 agency_name,
@@ -100,4 +149,4 @@ total_service_hits,
 total_agency_hits,
 'SEPTEMBER 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202109_report_detail` 
-WHERE regexp_contains(ga_id,'6986514')
+WHERE agency_name = 'HA';
