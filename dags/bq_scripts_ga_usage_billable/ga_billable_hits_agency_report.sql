74c25404e8f8e2c425d96490634b1c96deff370b
Mufaddal Taiyab-Ali <59077778+mufaddaltaiyeb@users.noreply.github.com>
2021-11-17 11:54:04 +1100

    Google Analytics usage report since january 2020 (#35)
    Co-authored-by: mufaddal <mufaddal.taiyab-ali@dta.gov.au>

--- dags/bq_scripts_ga_usage_billable/ga_billable_hits_agency_report.sql
+++ dags/bq_scripts_ga_usage_billable/ga_billable_hits_agency_report.sql
@@ -7,9 +7,69 @@ ga_id,
 service_name,
 total_service_hits,
 total_agency_hits,
+'JANUARY 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202001_report_detail` 
+-- WHERE agency_name = ''
+union all
+SELECT
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'FEBRUARY 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202002_report_detail` 
+-- WHERE agency_name = ''
+union all
+SELECT
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'MARCH 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202003_report_detail` 
+-- WHERE agency_name = ''
+union all
+SELECT
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'APRIL 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202004_report_detail` 
+-- WHERE agency_name = ''
+union all
+SELECT
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'MAY 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202005_report_detail` 
+-- WHERE agency_name = ''
+union all
+SELECT
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
+'JUNE 2020' as month_year
+FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202006_report_detail` 
+-- WHERE agency_name = ''
+union all
+SELECT
+agency_name,
+ga_id,
+service_name,
+total_service_hits,
+total_agency_hits,
 'JULY 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202007_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -19,7 +79,7 @@ total_service_hits,
 total_agency_hits,
 'AUGUST 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202008_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -29,7 +89,7 @@ total_service_hits,
 total_agency_hits,
 'SEPTEMBER 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202009_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -39,7 +99,7 @@ total_service_hits,
 total_agency_hits,
 'OCTOBER 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202010_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -49,7 +109,7 @@ total_service_hits,
 total_agency_hits,
 'NOVEMBER 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202011_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -59,7 +119,7 @@ total_service_hits,
 total_agency_hits,
 'DECEMBER 2020' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202012_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -69,7 +129,7 @@ total_service_hits,
 total_agency_hits,
 'JANUARY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202101_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -79,7 +139,7 @@ total_service_hits,
 total_agency_hits,
 'FEBRUARY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202102_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -89,7 +149,7 @@ total_service_hits,
 total_agency_hits,
 'MARCH 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202103_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -99,7 +159,7 @@ total_service_hits,
 total_agency_hits,
 'APRIL 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202104_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -109,7 +169,7 @@ total_service_hits,
 total_agency_hits,
 'MAY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202105_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -119,7 +179,7 @@ total_service_hits,
 total_agency_hits,
 'JUNE 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202106_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -129,7 +189,7 @@ total_service_hits,
 total_agency_hits,
 'JULY 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202107_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -139,7 +199,7 @@ total_service_hits,
 total_agency_hits,
 'AUGUST 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202108_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -149,7 +209,7 @@ total_service_hits,
 total_agency_hits,
 'SEPTEMBER 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202109_report_detail` 
-WHERE agency_name = 'Health'
+-- WHERE agency_name = ''
 union all
 SELECT 
 agency_name,
@@ -159,4 +219,5 @@ total_service_hits,
 total_agency_hits,
 'OCTOBER 2021' as month_year
 FROM `dta-ga-bigquery.dta_ga360_usage_billing.analytics_usage_202110_report_detail` 
-WHERE agency_name = 'Health';
+-- WHERE agency_name = ''
+;
